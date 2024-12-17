b9dd5bc5c2112ce6df616e510ac2746e0bb71fe3
MelodyDuplaix <melo.surseine@gmail.com>
2024-12-17 12:30:17 +0100

    Création d'un programme pour attribuer au hasard les groupes

--- test1.py
+++ test1.py
@@ -1,13 +1,30 @@
-import csv, pandas, os
+import csv
+import os
+import random
 from dotenv import load_dotenv
+import pandas as pd
+
+NB_PERSONNES_PAR_GROUPES = int(input("Combien de personnes par groupe ? "))
+
 
 load_dotenv()
-
 file = os.getenv('DATAFILE')
+df = pd.read_csv(file)
 
-csv = csv.reader(open(file, 'r'))
+nb_groupes = len(df) // NB_PERSONNES_PAR_GROUPES
+df["groupe"] = None
 
-for row in csv:
-    print(row)
+for i in range(nb_groupes):
+    group = df[df['groupe'].isnull()].sample(NB_PERSONNES_PAR_GROUPES)
+    group['groupe'] = i
+    df.loc[group.index, 'groupe'] = i
 
-print('OK')
+
+print(df)
+
+print("\n")
+
+for i in range(nb_groupes):
+    group = df[df['groupe'] == i]
+    print(f"Groupe {i}:")
+    print(group['Nom'].values)
