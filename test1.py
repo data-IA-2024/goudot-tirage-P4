fd8015a25b8c53a7a267c2fe6cad728b2632758d
MelodyDuplaix <melo.surseine@gmail.com>
2024-12-18 10:17:01 +0100

    Modification de la logique pour pouvoir répartir équitablement même avec un nombre non équilibré

--- test1.py
+++ test1.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import random
 import pandas as pd
 
 NB_PERSONNES_PAR_GROUPES = int(input("Combien de personnes par groupe ? "))
@@ -9,17 +10,18 @@ load_dotenv()
 file = os.getenv('DATAFILE')
 df = pd.read_csv(file)
 
-if len(df) % NB_PERSONNES_PAR_GROUPES != 0:
-    print("Le nombre de personnes doit être divisible par le nombre de groupes")
-    exit(1)
 
 nb_groupes = int(len(df) / NB_PERSONNES_PAR_GROUPES)
 df["groupe"] = None
 
 for i in range(nb_groupes):
     group = df[df['groupe'].isnull()].sample(NB_PERSONNES_PAR_GROUPES)
-    group['groupe'] = i
     df.loc[group.index, 'groupe'] = i
+
+while df['groupe'].isnull().any():
+    personne = df[df['groupe'].isnull()].sample(1)
+    df.loc[personne.index, 'groupe'] = df['groupe'].value_counts().idxmin()
+
 
 
 print(df)
