bda2d28bd1965cf15d63981939791febccfe4b7e
Emmanuel Goudot <Emmanuel.Goudot@gmail.com>
2024-12-18 10:17:35 +0100

    UPD tirage au sort OK

--- test1.py
+++ test1.py
@@ -1,7 +1,25 @@
-import csv, pandas, os
+import csv, os, random, sys
 from dotenv import load_dotenv
+import pandas as pd
 
 load_dotenv()  # take environment variables from .env.
 
-print('DATAFILE=', os.getenv('DATAFILE'))
-print('PATH', os.getenv('PATH'))
+DATAFILE = os.getenv('DATAFILE')
+print(f'{DATAFILE=}')
+print(f'{sys.argv=}')
+
+N = int(input('Taille des groupes ? '))
+print(f'{N=}')
+
+df = pd.read_csv(DATAFILE)
+df = df[df.type != 'prof']
+#print(df)
+
+noms = df['Nom']
+liste_nom = noms.values
+random.shuffle(liste_nom)
+print(liste_nom)
+
+for i in range(len(liste_nom)):
+    if i%N == 0 : print('----------------------')
+    print(liste_nom[i])
